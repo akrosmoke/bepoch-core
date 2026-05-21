112838202cab112687509417ec5f0df0a8bc7554
akrosmoke <139158377+akrosmoke@users.noreply.github.com>
2026-05-21 13:20:00 +0300

    feat: liveness probe, validation pipe, env template

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { EpochsModule } from './epochs/epochs.module';
 import { ModelsModule } from './models/models.module';
 import { ChainModule } from './chain/chain.module';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), EpochsModule, ModelsModule, ChainModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    EpochsModule,
+    ModelsModule,
+    ChainModule,
+  ],
+  controllers: [HealthController],
 })
 export class AppModule {}
